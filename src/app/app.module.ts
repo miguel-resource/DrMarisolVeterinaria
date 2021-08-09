0d9000e5f74901647288abafdfbaa99b4bcff041
Miguel Bermúdez <miguelbercru27@gmail.com>
2021-08-09 13:06:38 -0500

    Se agregó estilo de dirección en el footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,28 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { HeaderComponent } from './header/header.component';
 import { BlogComponent } from './blog/blog.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { LayoutComponent } from './layout/layout.component';
-import { FooterComponent } from './footer/footer.component';
+
+import {SharedModule} from './shared/shared.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     PageNotFoundComponent,
-    HeaderComponent,
     BlogComponent,
     AboutComponent,
     ContactComponent,
     LayoutComponent,
-    FooterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 //Declaramos nuestros componentes, directivas y pipes que van a quedar
-//compartidas.
- 
+//compartidas. 
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 @NgModule({
   declarations: [
-    //importamos nuestras declaraciones
+    FooterComponent,
+    HeaderComponent,
+  ],
+  exports:[
+    FooterComponent,
+    HeaderComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
   ]
 })
 export class SharedModule { }
